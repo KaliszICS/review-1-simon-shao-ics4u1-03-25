356cf71405fe4f8a335e005201b640a2456c524b
simon-shao-ics4u1-03-25 <simon.shao@student.tdsb.on.ca>
2025-02-04 19:52:56 +0000

    Answer Questions

--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -1,23 +1,38 @@
+
 public class Review1 {
 
 	public static void q1() {
 		//Write question 1 code here
+		System.out.println("There once was a man from St. Ives.");
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+		int Num1, Num2;
+		Num1 = 9;
+		Num2 = 5;
+		System.out.println(Num1/Num2);
+		System.out.println(Num1%Num2);
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+		System.out.println("Hello\nHello again");
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+		int Num =4;
+		System.out.println(Num == 4);
 	}
 
 	public static void q5() {
 		//Write question 5 code here
+		String word1 = "Hello";
+		String word2 = "World";
+System.out.print(word1 +" ");
+System.out.println(word2);
+
 	}
 
 }
